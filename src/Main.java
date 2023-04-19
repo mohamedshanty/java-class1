9fae8e18a3a4cf865c21f43e1369a7d61e286462
Mohamed salem <117261174+mohamedshanty@users.noreply.github.com>
2023-04-19 20:28:34 +0300

    Question 3

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Write a java program: ask the user to input the name and the age and print them
+        //1- Write a java program: ask the user to input the name and the age and print them
         /*Scanner input = new Scanner(System.in);
         System.out.println("Enter your name");
         String name = input.next();
@@ -11,8 +11,8 @@ public class Main {
         System.out.println("your name is: "+name + " and your age is :"+ age);*/
 
 
-        //Write a java program: ask the user to input two numbers and arrange it: Descending, Ascending
-        Scanner input = new Scanner(System.in);
+        //2- Write a java program: ask the user to input two numbers and arrange it: Descending, Ascending
+        /*Scanner input = new Scanner(System.in);
         System.out.println("Enter your first number:");
         int first = input.nextInt();
         System.out.println("Enter your second number");
@@ -20,7 +20,24 @@ public class Main {
         if (first > second)
             System.out.println(first + "is greater than" + second);
         else {
-            System.out.println(second + " is greater than " + first);
-        }
+            System.out.println(second + " is greater than " + first);*/
+
+
+
+         /*3- write a java program:
+        ask the user to input 4 numbers and print the average and the sum
+        without using arrays */
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your first number:");
+        int a = input.nextInt();
+        System.out.println("Enter your second number:");
+        int b = input.nextInt();
+        System.out.println("Enter your third number:");
+        int c = input.nextInt();
+        System.out.println("Enter your fourth number:");
+        int d = input.nextInt();
+        int sum = a + b + c + d;
+        int average = (a + b + c + d)/4;
+        System.out.println("the sum is: " + sum + " the average is: " + average);
     }
 }
